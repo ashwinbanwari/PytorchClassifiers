48bdf67672d779d0ff945e7ab14e43d411df7065
Ashwin Kishin Banwari <ashwinkbanwari@gmail.com>
2020-10-28 18:56:46 -0700

    Working MNIST, FashionMNIST with Dropout

--- MNIST.py
+++ MNIST.py
@@ -7,11 +7,11 @@ import model
 
 transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,),(0.5,))])
 # Download and load the training data
-trainset = datasets.FashionMNIST('~/.pytorch/F_MNIST_data/', download=True, train=True, transform=transform)
+trainset = datasets.MNIST('~/.pytorch/MNIST_data/', download=True, train=True, transform=transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
 
 # Download and load the test data
-testset = datasets.FashionMNIST('~/.pytorch/F_MNIST_data/', download=True, train=False, transform=transform)
+testset = datasets.MNIST('~/.pytorch/MNIST_data/', download=True, train=False, transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=64, shuffle=True)
 
 
