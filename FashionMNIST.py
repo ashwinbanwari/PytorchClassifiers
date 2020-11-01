6f61cc62a99e7d10676c61cd98e13370fb84140c
Ashwin Kishin Banwari <ashwinkbanwari@gmail.com>
2020-10-31 22:58:16 -0700

    Added random grayscale

--- FashionMNIST.py
+++ FashionMNIST.py
@@ -5,7 +5,7 @@ import model
 
 # normalize the data
 
-transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,),(0.5,))])
+transform = transforms.Compose([transforms.RandomGrayscale(), transforms.ToTensor(), transforms.Normalize((0.5,),(0.5,))])
 # Download and load the training data
 trainset = datasets.FashionMNIST('~/.pytorch/F_MNIST_data/', download=True, train=True, transform=transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
@@ -19,4 +19,3 @@ mod = model.Network(784, 10, [64])
 optimizer = optim.Adam(mod.parameters(), lr=0.003)
 criterion = nn.NLLLoss()
 model.train(mod, trainloader, testloader, criterion, optimizer)
-print('meow')
